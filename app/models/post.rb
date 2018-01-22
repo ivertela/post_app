e21faee606d4ac2f482aa17bb918c86dd42498b4
Ivan Vertela <i.vertela@dunice.net>
2018-01-22 16:12:33 +0300

    add friendly_id for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
+	extend FriendlyId
+	friendly_id :title, use: [:slugged, :finders]
 	validates_presence_of :title, :description
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180122124358) do
+ActiveRecord::Schema.define(version: 20180122130955) do
 
   create_table "friendly_id_slugs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "slug", null: false
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20180122124358) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
