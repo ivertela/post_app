352cb523bb61c00ca1b4aeedd1bd76954cf8ed4e
Ivan Vertela <i.vertela@dunice.net>
2018-01-22 16:02:40 +0300

    add login/logout for layout, and link for users/posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
   devise_for :users
   resources :users
+  resources :posts
+  root to: 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
